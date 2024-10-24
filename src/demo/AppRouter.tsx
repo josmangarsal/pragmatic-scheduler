2b7b9e74d0e7d482d101e2818a5529f19dc25517
Jose <11162684+josmangarsal@users.noreply.github.com>
2024-10-24 11:01:12 +0200

    DailyZoom demo, variable zoom level

--- src/demo/AppRouter.tsx
+++ src/demo/AppRouter.tsx
@@ -5,15 +5,17 @@ import Monthly from './Monthly';
 import Weekly from './Weekly';
 import { Navigate } from 'react-router';
 import Daily2 from './Daily2';
+import DailyZoom from './DailyZoom';
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/daily" element={<Daily />} />
       <Route path="/daily2" element={<Daily2 />} />
+      <Route path="/dailyZoom" element={<DailyZoom />} />
       <Route path="/week" element={<Weekly />} />
       <Route path="/month" element={<Monthly />} />
-      <Route path="*" element={<Navigate replace to="/daily" />} />
+      <Route path="*" element={<Navigate replace to="/dailyZoom" />} />
     </Routes>
   );
 };
