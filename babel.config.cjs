7d09c198bd2ddbe898bc927c10ac2423c95fb11c
Jose <jmgarcia@tedial.com>
2025-06-13 09:59:35 +0200

    Testing

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,4 +1,8 @@
 // export const presets = ['@babel/preset-env', '@babel/preset-react'];
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+  presets: [
+    ['@babel/preset-env', {targets: {node: 'current'}}],
+    ['@babel/preset-react', {runtime: 'automatic', importSource: '@emotion/react'}],
+    '@babel/preset-typescript',
+  ],
 };
--- jest-setup.ts
+++ jest-setup.ts
@@ -3,3 +3,25 @@ import '@testing-library/jest-dom';
 import React from 'react';
 global.React = React;
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
+
+class ResizeObserverMock {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  observe() { }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  unobserve() { }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  disconnect() { }
+}
+
+global.ResizeObserver = ResizeObserverMock as any;
+
+class IntersectionObserverMock {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  observe() { }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  unobserve() { }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  disconnect() { }
+}
+
+global.IntersectionObserver = IntersectionObserverMock as any;
